2b674b67ade47983034fe524672311200dc7d040
brbe6228 <brbe6228@colorado.edu>
2026-01-22 18:44:20 -0700

    Update HW1_problem4.m

--- HW1_problem4.m
+++ HW1_problem4.m
@@ -1,1 +1,18 @@
+clc; clear;
 
+N = 1e4;
+X = rand(N,3);
+T = max(X,[],2);
+
+figure;
+histogram(T,'Normalization','pdf');
+hold on;
+
+t = linspace(0,1,500);
+plot(t,3*t.^2,'LineWidth',2);
+
+xlabel('t');
+ylabel('density');
+title('Simulation vs Theory');
+legend('Histogram','3t^2');
+grid on;
